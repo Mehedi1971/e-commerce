dec4cce7afd1a96d6c7a6f9617ec8d0956c5005d
Md. Mahedi Hasan <82325267+Mehedi1971@users.noreply.github.com>
2023-09-11 18:07:41 +0600

    [ADD] Validation

--- src/main/java/com/mahedi/ecommerce/controller/RegistrationController.java
+++ src/main/java/com/mahedi/ecommerce/controller/RegistrationController.java
@@ -3,7 +3,9 @@ package com.mahedi.ecommerce.controller;
 import com.mahedi.ecommerce.model.Registration;
 import com.mahedi.ecommerce.service.RegistrationService;
 import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +17,7 @@ public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping
-    public String saveUser(@RequestBody Registration registration) throws MessagingException {
+    public String saveUser(@Valid @RequestBody Registration registration) throws MessagingException {
         return registrationService.saveUser(registration);
     }
 
--- src/main/java/com/mahedi/ecommerce/model/Registration.java
+++ src/main/java/com/mahedi/ecommerce/model/Registration.java
@@ -1,9 +1,13 @@
 package com.mahedi.ecommerce.model;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Data
@@ -12,8 +16,11 @@ import lombok.NoArgsConstructor;
 public class Registration extends BaseModel {
     private String name;
     private String address;
+    @NotEmpty
     private String phone;
     private String email;
+    @NotBlank(message = "This field is mandatory")
+    @Size(min = 4, max = 8,message = "")
     private String password;
     private String verificationNumber;
 }
