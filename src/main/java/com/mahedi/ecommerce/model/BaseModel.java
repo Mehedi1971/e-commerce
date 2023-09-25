dfade8f1c02995a6b3671f28faecf4127e3fc25c
Md. Mahedi Hasan <82325267+Mehedi1971@users.noreply.github.com>
2023-09-25 09:55:01 +0600

    [ADD] add more functionality

--- src/main/java/com/mahedi/ecommerce/model/BaseModel.java
+++ src/main/java/com/mahedi/ecommerce/model/BaseModel.java
@@ -1,6 +1,15 @@
 package com.mahedi.ecommerce.model;
 
-import jakarta.persistence.*;
+import com.mahedi.ecommerce.enums.ActiveStatus;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +19,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @MappedSuperclass
 public class BaseModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private int activeStatus;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private int activeStatus;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdAt;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date updatedAt;
+
+  @PrePersist
+  public void setPerInsertData() {
+    this.createdAt = new Date();
+//    if(this.activeStatus==null){
+//      this.activeStatus= ActiveStatus.ACTIVE.getValue();
+//    }
+  }
+
+  @PreUpdate
+  public void setPreUpdateData(){
+    this.updatedAt=new Date();
+
+  }
+
 }
+
+
+
+
